8f145ac6acdc8292e0cda6e160eb866d1651e646
AlexSNIC <alexandru13321@gmail.com>
2026-03-20 14:34:56 +0200

    contact correction

--- src/components/HyperLinks.jsx
+++ src/components/HyperLinks.jsx
@@ -4,7 +4,7 @@ const HyperLinks = (props) => {
   return (
     <div {...props} className='hyperlinks'>
       <ul>
-        <li><a target='_blank' href="https://github.com/Alex-LikesPizza"><i className="bi bi-github"></i>Alex-LikesPizza</a></li>
+        <li><a target='_blank' href="https://github.com/AlexSNIC"><i className="bi bi-github"></i>AlexSNIC</a></li>
         <li><a target='_blank' href="https://www.instagram.com/alexandry_solomon/"><i className="bi bi-instagram"></i>@alexandry_solomon</a></li>
         <li><a target='_blank' href="https://www.facebook.com/alexandru.solomon.716"><i className="bi bi-facebook"></i>alexandru.solomon.716</a></li>
       </ul>
